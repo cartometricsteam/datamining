007d0ac2a72b18c504b02dae535abeb8e6e028d4
Rubén Fernández Gordillo <7360184+rubfergor@users.noreply.github.com>
2017-09-21 14:29:33 +0200

    Add the auth for twitter

--- main.js
+++ main.js
@@ -2,4 +2,17 @@
 /*
 API keys  go into the .env file in the format {SOCIAL NETWORK}_{KEY CLASS}.
 */
-const dotenv = require('dotenv').config();
+const dotenv = require("dotenv").config(),
+      Twitter = require("twitter");
+
+
+/*
+Creates a Twitter instances and give it our account credential (which, as
+you alredy know, are in the .env file)
+*/
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
