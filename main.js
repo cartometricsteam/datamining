42ef61961593e14f3dab5a2dcf0d45ae46a4a7cb
Rubén Fernández Gordillo <7360184+rubfergor@users.noreply.github.com>
2017-09-21 14:13:11 +0200

    Added a require for .env and created a .env file. This closes #5

---
+++ main.js
@@ -0,0 +1,6 @@
+// # REQUIRES
+/*
+Consumer keys and secrets go into the .env file in the format
+{SOCIAL_NETWORK}_CONSUMER_{KEY|SECRET} and ALWAYS as strings.
+*/
+const dotenv = require('dotenv').config();
