71bb9fbb1c4694143725bf45a77803ac5ae73b35
Rubén Fernández Gordillo <7360184+rubfergor@users.noreply.github.com>
2017-09-22 00:03:59 +0200

    Added a CLI functionality to pass the tokens

--- main.js
+++ main.js
@@ -4,12 +4,18 @@ API keys  go into the .env file in the format {SOCIAL NETWORK}_{KEY CLASS}.
 */
 const dotenv = require("dotenv").config(),
       Twitter = require("twitter"),
-      argv = require("yarg"),
-      streamSuccess = require("./lib/filter"),
+      argv = require("yargs")
+             .usage('Usage: $0 --filter=[str] --keyword=[str]')
+             .option( "f", { alias: "filter", demand: true, describe: "Filter keyword for the Twitter API", type: "string" } )
+             .option( "k", { alias: "keyword", demand: true, describe: "Keyword you want to use for filtering", type: "string" } )
+             .example("$0 -f \"track\" -k \"javascript\" ", "Get tweets about Javascript")
+             .epilog( "Copyright (c) 2017 Cartometrics under MIT License" )
+             .argv,
+      streamFilter = require("./lib/filter"),
       streamError = require("./lib/error");
 
 
-// # ENV VARIABLES
+// # VARIABLES
 /*
 Creates a Twitter instances and give it our account credential (which, as
 you alredy know, are in the .env file)
@@ -20,12 +26,25 @@ const client = new Twitter({
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
+/*
+Will be passed to the Twitter stream as the filtering token of the API.
+*/
+const token = {};
+
 
 // # MAIN LOGIC
+
 /*
-Connect to Twitter stream with the user credentials.
+Construct the token from the arguments given.
 */
-client.stream("statuses/filter", argv.filter, function (stream) {
+token[argv.filter] = argv.keyword;
+
+/*
+Connect to Twitter stream with the user credentials and the given token
+*/
+
+client.stream("statuses/filter", token, function (stream) {
   stream.on("data", streamFilter);
   stream.on("error", streamError);
 });
+
